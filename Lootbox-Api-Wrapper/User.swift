4f07db056afe7042a79609015a017362d0dfbc39
Matt Rocco <roccoma504@gmail.com>
2016-08-12 14:42:27 -0400

    finished user

--- Lootbox-Api-Wrapper/User.swift
+++ Lootbox-Api-Wrapper/User.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class User: NSObject {
     
+    enum ValueKind : String {
+        case name
+        case level
+        case rank
+        case rankImg
+        case levelFrame
+        case star
+        case quickWins
+        case quickLost
+        case quickPlayed
+        case quickPlayTime
+        case competitiveWins
+        case competitiveLost
+        case competitivePlayed
+        case competitivePlaytime
+        case avatar
+        
+    }
     
     private var ID : String
     private var platform : PlatformType
     private var region : RegionType
-    
     private var json : [String : AnyObject]!
     
     /**
@@ -48,6 +65,49 @@ class User: NSObject {
                 self.json = json
                 completion(success: true, error: error)
             }
+        }
+    }
+    
+    /**
+     Returns the specifified value.
+     
+     - parameter kind: Possible value kinds
+     
+     - returns: A String representing the value in the JSON specified by
+     the input parameter
+     */
+    func get(kind : ValueKind) -> String {
+        switch kind {
+        case .name:
+            return String(json["username"])
+        case .level:
+            return String(json["level"])
+        case .quickWins:
+            return String(json["quick"]!["wins"])
+        case .quickLost:
+            return String(json["quick"]!["lost"])
+        case .quickPlayed:
+            return String(json["quick"]!["played"])
+        case .quickPlayTime:
+            return String(json["playtime"]!["quick"])
+        case .competitiveWins:
+            return String(json["competitive"]!["wins"])
+        case .competitiveLost:
+            return String(json["competitive"]!["lost"])
+        case .competitivePlayed:
+            return String(json["competitive"]!["played"])
+        case .competitivePlaytime:
+            return String(json["playtime"]!["competitive"])
+        case .avatar:
+            return String(json["avatar"])
+        case .rank:
+            return String(json["competitive"]!["rank"])
+        case .rankImg:
+            return String(json["competitive"]!["rank_img"])
+        case .levelFrame:
+            return String(json["levelFrame"])
+        case .star:
+            return String(json["star"])
         }
     }
     
